bc96bd663ece4295152d69b398ca2d008eaf3e0c
Kostiuk93 <kostiuk93@gmail.com>
2022-03-30 16:55:53 +0300

    Практический курс js Проект #2 Маски и проверка текста (доп)

--- dist/script.js
+++ dist/script.js
@@ -4195,10 +4195,12 @@ __webpack_require__.r(__webpack_exports__);
 var checkTextInput = function checkTextInput(selector) {
   var txtInputs = document.querySelectorAll(selector);
   txtInputs.forEach(function (input) {
+    // Ввод в интут только кирилицей и цифрами
     input.addEventListener('keypress', function (e) {
       if (e.key.match(/[^а-яё 0-9]/ig)) {
         e.preventDefault();
-      }
+      } // Удаляет автозаполнение инпута латиницей
+
 
       input.addEventListener('input', function () {
         if (input.value.match(/[a-z]/ig)) {
--- src/js/modules/checkTextInput.js
+++ src/js/modules/checkTextInput.js
@@ -2,10 +2,13 @@ const checkTextInput = (selector) => {
     const txtInputs = document.querySelectorAll(selector);
 
     txtInputs.forEach(input => {
+        // Ввод в интут только кирилицей и цифрами
         input.addEventListener('keypress', function(e) {
             if (e.key.match(/[^а-яё 0-9]/ig)) {
                 e.preventDefault();
             }
+
+        // Удаляет автозаполнение инпута латиницей
         input.addEventListener('input', () => {
             if (input.value.match(/[a-z]/ig)) {
                 input.value = '';

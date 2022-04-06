13a444e9498f34a642e1a5331cd65a9030268e37
Kostiuk93 <kostiuk93@gmail.com>
2022-04-06 23:03:00 +0300

    Практический курс js Проект #2 Аккордеон на JS и CSS (Лучший вариант)

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,21 +1,28 @@
-const accordion = (triggersSelector, itemsSelector) =>{
-    const btns = document.querySelectorAll(triggersSelector),
-          blocks = document.querySelectorAll(itemsSelector);
-
-    blocks.forEach(block =>{
-        block.classList.add('animated', 'fadeInDown');
-    });
-
+const accordion = (triggersSelector) =>{
+    const btns = document.querySelectorAll(triggersSelector);
+    
     btns.forEach(btn => {
         btn.addEventListener('click', function() {
-            if (!this.classList.contains('active')) {
-                btns.forEach(btn => {
-                    btn.classList.remove('active', 'active-style');
-                });
-                this.classList.add('active', 'active-style');
+            
+            // Скрыть все элементы аккордеона, чтобы открывалась только одна вкладка
+            btns.forEach(btn => {
+                btn.classList.remove('active-style');
+                btn.nextElementSibling.classList.remove('active-content');
+                btn.nextElementSibling.style.maxHeight = '0px';
+            });
+
+            // Расскрывает одну вкладку аккордеона
+            this.classList.toggle('active-style');
+            this.nextElementSibling.classList.toggle('active-content');
+
+            if (this.classList.contains('active-style')) {
+                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
+                } else {
+                this.nextElementSibling.style.maxHeight = '0px';
             }
         });
     });
+    
 };
 
 export default accordion;
